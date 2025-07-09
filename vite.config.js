84b71a6235421422d1f9e4f652926d4242869d2c
Milan Parmar <parmarmilan089@gmail.com>
2025-07-09 13:39:56 +0530

    adding the conflict 1

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+        hmr: {
+            host: '127.0.0.1',
+            port: 5173,
+        },
+    },
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
     ],
 });
