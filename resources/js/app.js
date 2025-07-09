4923d824ad3bdd3287acae2fb83aa19ae5d358bb
Milan Parmar <parmarmilan089@gmail.com>
2025-07-09 13:41:09 +0530

    addin conflict 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,7 @@ import { createApp } from 'vue';
 import InventoryCreate from './components/InventoryCreate.vue';
 // import UserProfile from './components/UserProfile.vue';
 
-<<<<<<< HEAD
-const app = createApp({});
-app.component('inventory-create', InventoryCreate);
-app.mount('#app');
-=======
 console.log('Vue app loading...');
-
 // Helper to mount any component to a given DOM element
 function mountComponent(selector, component) {
     const el = document.querySelector(selector);
@@ -20,4 +14,3 @@ function mountComponent(selector, component) {
 // Mount components conditionally
 mountComponent('#inventory-create', InventoryCreate);
 // mountComponent('#user-profile', UserProfile);
->>>>>>> 9d23e93 (implement the vue js)
